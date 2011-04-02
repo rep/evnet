6d5bfbb41c0f228bb4848461cc88bc36bd56eb34
Mark Schloesser <ms@mwcollect.org>
2011-04-02 16:31:00 +0200

    updated setup.py to include long_description

--- setup.py
+++ setup.py
@@ -3,16 +3,23 @@ distribute_setup.use_setuptools()
 
 from setuptools import setup
 
-import sys
+import sys, os
+
+def read_file(name):
+	return open(os.path.join(os.path.dirname(__file__),name)).read()
 
 extra = {}
 if sys.version_info >= (3,):
 	extra['use_2to3'] = True
 
+readme = read_file('README.md')
+
 setup(
 	name='evnet',
-	version = '1.0-1',
+	version = '1.0-3',
 	description='evnet is a networking library built on top of pyev (libev)',
+	long_description=readme,
+	classifiers=['Development Status :: 4 - Beta','License :: OSI Approved :: MIT License','Programming Language :: Python','Topic :: Software Development :: Libraries :: Python Modules'], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
 	author='Mark Schloesser',
 	author_email='ms@mwcollect.org',
 	license = "MIT/BSD/GPL",
